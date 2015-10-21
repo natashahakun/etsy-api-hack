1f685d51b877aa3a6c4f9b0057c509f58f96918a
natashalynne <hakun.natasha@gmail.com>
2015-10-21 19:46:42 -0400

    Put appending functionality into a separate function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
 $(document).ready(function() {
 
-  $(".submit").click(function() {
-    var newMatch = $("<div></div>", {class: "individual-match"})
-  })
-    $(".matches-container").append(newMatch);
-
-    var newThumbnail = $("<div></div>", {class: "thumbnail"})
-    $(newMatch).append(newThumbnail);
-    var newLink = $("<div></div>", {class: "link"})
-    $(newMatch).append(newLink);
+  $(".submit").click(function(event) {
+    event.preventDefault();
+    var searchTerm = $(".search-items").val();
+    getRequest(searchTerm);
 
     $(".search-items").val('');
     $(".search-shop").val('');
+  })
+
 });
+
+function getRequest(searchTerm){
+  var params = {
+    part: "",
+    key: "",
+    q:
+  };
+  url = "";
+
+  $.getJSON(url, params, function(data){
+    showResults(data);
+  });
+}
+
+function showResults(results) {
+  var newMatch = $("<div></div>", {class: "individual-match"});
+  $(".matches").append(newMatch);
+  var newThumbnail = $("<div></div>", {class: "thumbnail"})
+  $(newMatch).append(newThumbnail);
+  var newLink = $("<div></div>", {class: "link"})
+  $(newMatch).append(newLink);
+}
+
