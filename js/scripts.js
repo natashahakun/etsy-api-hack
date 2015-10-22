942c2cc31aa518a55ca1a6bae2ea2dd00cb30115
natashalynne <hakun.natasha@gmail.com>
2015-10-22 15:08:59 -0400

    Shows images when searched.

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,7 @@ function getRequest(searchTerm){
     dataType: "jsonp",
     success: function(data) {
       showResults(data.results);
+      console.log(data.results);
     },
     error: function(data){
       console.log(data,'error');
@@ -38,15 +39,38 @@ function getRequest(searchTerm){
 }
 
 function showResults(results) {
-  for (i = 0; i < results.length; i++) {
-    var newMatch = $("<div></div>", {class: "individual-match"});
-    $(".matches").append(newMatch);
-    var newThumbnail = $("<div></div>", {class: "thumbnail"})
-    $(newMatch).append(newThumbnail);
-    var newLink = $("<div></div>", {class: "link"})
-    $(newMatch).append(newLink);
-    var text = results[i].title;
-    $(newLink).append(text);
+  for (i = 0; i < 10; i++) {
+
+ var params = {
+      api_key: "c03v0gzczhnbj4b0rw4aaoh0"
+    };
+
+    $.ajax({
+    url: "https://openapi.etsy.com/v2/listings/" + results[i].listing_id + "/images.js",
+    data: params,
+    type: "GET",
+    dataType: "jsonp",
+    success: function(data) {
+      var newMatch = $("<div></div>", {class: "individual-match"});
+      $(".matches").append(newMatch);
+
+      var newThumbnail = $("<div></div>", {class: "thumbnail"})
+      $(newMatch).append(newThumbnail);
+
+      var newLink = $("<div></div>", {class: "link"})
+      $(newMatch).append(newLink);
+
+      var text = results[i].title;
+      $(newLink).append(text);
+
+      var img = $("<img src=" + '"' + data.results[0].url_75x75 + '"' + " />");
+      $(newThumbnail).append(img);
+
+      },
+    error: function(data){
+      console.log(data,'error');
+    }
+  })
   }
 }
 
